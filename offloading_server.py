e04c1ea3a944184d82d37067f8af4ac9dbb6b426
None <None>
2017-09-08 04:58:15 -0700

    add offloading server

---
+++ offloading_server.py
@@ -0,0 +1,43 @@
+import  protobuf.messages_pb2 as msg_pb2
+import os
+import socketserver
+
+
+MESSAGE_TYPES = {'offload_request': msg_pb2.OffloadRequest()}
+
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    def handle(self, *args):
+
+        message = self.read_message()
+        print("{} wrote:".format(self.client_address[0]))
+        print(message)
+        #self.server.ryu_app.send_event_to_observers(EventMsg('New Policy request'))
+        # just send back the same data, but upper-cased
+        self.data = bytes('ciao', 'utf-8')
+        self.request.sendall(self.data.upper())
+
+    def send_message(self, message):
+        size = string(message.ByteSize())
+        self.request.sendall(bytes(size + '\n'))
+        self.request.sendall(message.SerializeToString())
+
+    def read_message(self,):
+        self.data = self.rfile.readline().strip()
+
+        try:
+            msg_size = int(self.data)
+        except ValueError:
+            print('not a valid message size')
+        print('expected message size', msg_size)
+        message_s = self.request.recv(msg_size)
+        message = MESSAGE_TYPES['offload_request']
+        message.ParseFromString(message_s)
+        return message
+
+def main():
+	HOST, PORT = "localhost", 9999
+	server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+	server.serve_forever()
+
+if __name__ == '__main__':
+	main()
