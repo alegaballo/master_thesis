071f98f283accc58ea7ea9cdc08af1678eab9c28
Alessandro Gaballo <alessandrogbl@gmail.com>
2017-12-22 15:56:21 -0600

    first protocol implementation

--- edge_server.py
+++ edge_server.py
@@ -1,20 +1,52 @@
-import socket
+import protobuf.messages_pb2 as msg_pb2
 import socketserver
+
+RESPONSES = {'OK': msg_pb2.Response.OK, 'INVALID_SIZE': msg_pb2.Response.INVALID_MSG_SIZE,
+             'INVALID_REQUEST': msg_pb2.Response.INVALID_REQUEST}
 
 
 class TCPHandler(socketserver.StreamRequestHandler):
-    def handle(self):
-        msg_size = self.rfile.readline().strip()
-        print(msg_size)
-        try:
-            msg_size = int(msg_size)
-        except ValueError:
-            # send error response
-            print(' ehhhhhhhhh volevi')
-            return
+    def setup(self):
+        # calling superclass method which initialize rfile
+        super(TCPHandler, self).setup()
+        self.SEND_MSGS = {'response': self.send_response}
 
-    def read_message(self, msg_size):
-        pass
+    def handle(self):
+        message = self.read_message()
+
+    def read_message(self):
+        # reading incoming message size
+        self.data = self.rfile.readline().strip()
+
+        try:
+            msg_size = int(self.data)
+        except ValueError:
+            print('not a valid message size')
+            self.send_message('response', response=RESPONSES['INVALID_SIZE'], msg='{:} is not a valid msg size'.format(self.data))
+            return
+            # send error message
+        message = msg_pb2.Message()
+        print('Expected message size', msg_size)
+        self.send_message('response', response=RESPONSES['OK'], msg='OK')
+
+        return self.receive_message(msg_size)
+
+    def receive_message(self, msg_size):
+        received = 0
+        message_s = bytes()
+        while received != msg_size:
+
+            message_s += self.request.recv(msg_size - received)
+            received = len(message_s)
+
+        print('Received message size: {:}'.format(len(message_s)))
+        message = msg_pb2.Message()
+        message.ParseFromString(message_s)
+
+        return message
+
+    def send_message(self, MESSAGE_TYPE, **kwargs):
+        self.SEND_MSGS[MESSAGE_TYPE](**kwargs)
 
 
 if __name__ == "__main__":
