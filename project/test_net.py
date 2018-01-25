104df6f1413af62e5c7276c596c0e8740d98bf88
alegaballo <alessandrogbl@gmail.com>
2018-01-25 09:04:45 -0600

    adapt for testing

--- project/test_net.py
+++ project/test_net.py
@@ -35,9 +35,9 @@ blacklist = [('r2', 'ri3'), ('r2', 'ri4'), ('r3', 'r6'), ('r4', 'r1'), ('r4', 'r
 
 DEF_PSW = 'zebra'
 REF_BANDWIDTH = 1000
-SIM_DURATION = 12000000000000000000000000000000000000000000000 # seconds of traffic simulation duration
+SIM_DURATION = 12 #seconds of traffic simulation duration
 TRAFFIC_PROB = 0.65
-ITERATION = 15
+ITERATION = 2
 net = None
 
 
